419632eaeb1937a9d152a735b5965378a59e5928
iimaanhashim <khaanbarwaani@gmail.com>
2025-03-07 19:38:39 +0300

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://stuck-swordfish-khaan-6ca517bd.koyeb.app/")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002308375163'))
