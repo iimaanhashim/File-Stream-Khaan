8c7ac205823b0cc23a4893b6b36210fd7cd79a3a
iimaanhashim <khaanbarwaani@gmail.com>
2025-05-29 12:18:22 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     rm = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("✨ Update Channel", url="https://t.me/khaanfilmsmovies")
+            InlineKeyboardButton("✨ Update Channel", url="https://t.me/+c5jctZy9axQyOGFk")
         ]] 
     )
     await client.send_message(
